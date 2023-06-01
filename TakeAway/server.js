c614fe899b1e382cbb98de0af8896f9d1eb7633f
trungb19 <101488122+trungb19@users.noreply.github.com>
2023-06-01 20:57:42 +0700

    Delete TakeAway

--- TakeAway/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const config = require("./app/config");
-
-// Khởi tạo server
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server in running on post ${PORT}`);
-});
